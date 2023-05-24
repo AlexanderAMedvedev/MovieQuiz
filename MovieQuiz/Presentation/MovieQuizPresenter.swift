1d459220c8914967144710f515238de79dcbcc97
Alexander <vinsanity305@mail.ru>
2023-05-25 00:06:25 +0300

    Done: great part of refactoring of Massive ViewController to MVP architecture. For: to remove business logic from MovieQuizViewController.

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -9,11 +9,23 @@ import Foundation
 import UIKit
 
 final class MovieQuizPresenter {
-    weak var viewController: MovieQuizViewController?
+    var questionFactory: QuestionFactoryProtocol?
+    private let statistics: StatisticsService!
+    private weak var viewController: MovieQuizViewController?
     let questionsAmount: Int = 10
     var currentQuestion: QuizQuestion?
     private var currentQuestionIndex: Int = 0 // переменная с индексом текущего вопроса
+    var correctAnswers: Int = 0
+    
+    init(viewController: MovieQuizViewController) {
+        self.viewController = viewController
+        statistics = StatisticsServiceImplementation()
+        
+        questionFactory = QuestionFactory(delegate: self, moviesLoader: MoviesLoader())
+        questionFactory?.loadData()
+        viewController.showLoadingIndicator()
 
+    }
     func isLastQuestion() -> Bool {
             currentQuestionIndex == questionsAmount - 1
     }
@@ -31,18 +43,81 @@ final class MovieQuizPresenter {
               questionNumber: "\(currentQuestionIndex+1)/\(questionsAmount)")
         return questionStep
     }
-    func noButtonClicked() {
-        let userAnswer: Bool = false
+    private func didAnswer(isYes: Bool) {
+        let userAnswer: Bool = isYes
         guard let currentQuestion = currentQuestion else {
             return }
-        
         viewController?.showAnswerResult(isCorrect: userAnswer == currentQuestion.correctAnswer)
     }
+    func noButtonClicked() {
+        didAnswer(isYes: false)
+    }
     func yesButtonClicked() {
-        guard let currentQuestion = currentQuestion else {
-        return }
-        let userAnswer: Bool = true
-        
-        viewController?.showAnswerResult(isCorrect: userAnswer == currentQuestion.correctAnswer)
+        didAnswer(isYes: true)
+    }
+     func showNextQuestionOrResults() {
+        //переход в один из сценариев
+        if self.isLastQuestion() {
+// состояние "Результат квиза"
+//1 Предварительная подготовка параметров модели "QuizResultsViewModel"
+            //распаковка опционала, чтобы далее код был проще
+            guard let statistics = statistics else {
+                print("statistics object error")
+                return}
+         // 1 рекорд
+          // 1 сохранение лучшей игры
+              // 1 запись текущей игры
+            let currentGame = GameRecord(correct: correctAnswers, total: self.questionsAmount, date: Date())
+              // 2 запись лучшей игры на устройство
+            statistics.setBestGame(currentGame)
+          // 2 сам рекорд - запись о лучшей игре
+            let record: String = """
+                                 \(statistics.bestGame.correct)/\(statistics.bestGame.total) \
+                                 (\(statistics.bestGame.date.dateTimeString))
+                                 """
+        //2 средняя точность
+            //1 учесть в статистике по всем играм кол-во всех ответов, кол-во только правильных ответов, которые даны в последней игре
+            statistics.store(totalCurrent: self.questionsAmount, correctCurrent: correctAnswers)
+            //2 получить значение в виде, требуемом макетом
+            let averageAccuracy = String(format: "%.2f", statistics.totalAccuracy)
+//2 Создание объекта "QuizResultsViewModel"
+                let result = QuizResultsViewModel(
+                    title: "Этот раунд окончен!",
+                    text: """
+                          Ваш результат: \(correctAnswers)/\(self.questionsAmount)
+                          Количество сыгранных квизов: \(statistics.gamesCount)
+                          Рекорд: \(record)
+                          Средняя точность: \(averageAccuracy)%
+                          """,
+                          buttonText: "Сыграть ещё раз"
+                )
+//3 Показ объекта "QuizResultsViewModel" на экране
+            viewController?.show(quiz: result)
+        } else {
+            //состояние "Следующий вопрос показан"
+            self.switchToNextQuestion()
+            questionFactory?.requestNextQuestion()
+        }
+    }
+}
+extension MovieQuizPresenter: QuestionFactoryDelegate {
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        guard let question = question else {
+            return
+        }
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        DispatchQueue.main.async { [weak self] in
+            self?.viewController?.show(quiz: viewModel)
+            }
+    }
+    func didLoadDataFromServer() {
+        viewController?.downloadMoviesIndicator.isHidden = true // скрываем индикатор загрузки
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func didFailToLoadData(with error: Error) {
+        viewController?.showNetworkError(message: error.localizedDescription)
+        //localizedDescription - Retrieve(Извлекать) the localized description for this error.
     }
 }
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -8,43 +8,35 @@ final class MovieQuizViewController: UIViewController {
     // 3) Что значит "final class"?
     //4 Can I leave some commands, needed only for iOS-developer? (like `printSandBox()`)
     //! End of `Discuss`
-    private let presenter = MovieQuizPresenter()
+    private var presenter: MovieQuizPresenter!
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    private var currentQuestion: QuizQuestion?
-    private var correctAnswers = 0 // переменная с количеством правильных ответов
-    private var questionFactory: QuestionFactoryProtocol?
     private var alertView: AlertPresenterProtocol?
-    private var statistics: StatisticsService?
     @IBOutlet private weak var infoLabel: UILabel!
     @IBOutlet private weak var counterLabel: UILabel!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var titleNoButton: UIButton!
     @IBOutlet private weak var titleYesButton: UIButton!
-    @IBOutlet private weak var downloadMoviesIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var downloadMoviesIndicator: UIActivityIndicatorView!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter.viewController = self
-        questionFactory = QuestionFactory(delegate: self, moviesLoader: MoviesLoader())
-        showLoadingIndicator()
-        questionFactory?.loadData()
+        presenter = MovieQuizPresenter(viewController: self)
         infoLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
         counterLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
         textLabel.numberOfLines = 2
         textLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
         titleNoButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
         titleYesButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
-        statistics = StatisticsServiceImplementation()
 //вывод первого вопроса на экран в рамках паттерна "Делегат" в случае локальных данных
        // questionFactory?.requestNextQuestion()
 // Only for finding the path to sandBox: statistics?.printSandBox()
     }
     /// Вывод на экран вопроса (принимает  ViewModel вопроса)
-    private func show(quiz step: QuizStepViewModel) {
+    func show(quiz step: QuizStepViewModel) {
         counterLabel.text = step.questionNumber
         imageView.image = step.image
         imageView.layer.masksToBounds = true // даём разрешение на рамку
@@ -60,7 +52,7 @@ final class MovieQuizViewController: UIViewController {
     // метод красит рамку
         imageView.layer.borderWidth = 8  // толщина рамки
         if isCorrect {
-            correctAnswers += 1
+            presenter.correctAnswers += 1
             imageView.layer.borderColor = UIColor.ypGreen.cgColor
         } else {
             imageView.layer.borderColor = UIColor.ypRed.cgColor
@@ -68,63 +60,20 @@ final class MovieQuizViewController: UIViewController {
         // запускаем задачу через 1 секунду c помощью диспетчера задач
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
             guard let self = self else { return }
-            self.showNextQuestionOrResults()
+            self.presenter.showNextQuestionOrResults()
         }
     }
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         sender.isEnabled = false
-        presenter.currentQuestion = currentQuestion
         presenter.noButtonClicked()
     }
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         sender.isEnabled = false
-        presenter.currentQuestion = currentQuestion
         presenter.yesButtonClicked()
     }
-    private func showNextQuestionOrResults() {
-        //переход в один из сценариев
-        if presenter.isLastQuestion() {
-// состояние "Результат квиза"
-//1 Предварительная подготовка параметров модели "QuizResultsViewModel"
-            //распаковка опционала, чтобы далее код был проще
-            guard let statistics = statistics else { print("statistics object error"); return}
-         // 1 рекорд
-          // 1 сохранение лучшей игры
-              // 1 запись текущей игры
-            let currentGame = GameRecord(correct: correctAnswers, total: presenter.questionsAmount, date: Date())
-              // 2 запись лучшей игры на устройство
-            statistics.setBestGame(currentGame)
-          // 2 сам рекорд - запись о лучшей игре
-            let record: String = """
-                                 \(statistics.bestGame.correct)/\(statistics.bestGame.total) \
-                                 (\(statistics.bestGame.date.dateTimeString))
-                                 """
-        //2 средняя точность
-            //1 учесть в статистике по всем играм кол-во всех ответов, кол-во только правильных ответов, которые даны в последней игре
-            statistics.store(totalCurrent: presenter.questionsAmount, correctCurrent: correctAnswers)
-            //2 получить значение в виде, требуемом макетом
-            let averageAccuracy = String(format: "%.2f", statistics.totalAccuracy)
-//2 Создание объекта "QuizResultsViewModel"
-                let result = QuizResultsViewModel(
-                    title: "Этот раунд окончен!",
-                    text: """
-                          Ваш результат: \(correctAnswers)/\(presenter.questionsAmount)
-                          Количество сыгранных квизов: \(statistics.gamesCount)
-                          Рекорд: \(record)
-                          Средняя точность: \(averageAccuracy)%
-                          """,
-                          buttonText: "Сыграть ещё раз"
-                )
-//3 Показ объекта "QuizResultsViewModel" на экране
-                show(quiz: result)
-        } else {
-            //состояние "Следующий вопрос показан"
-            presenter.switchToNextQuestion()
-            questionFactory?.requestNextQuestion()
-        }
-    }
+
     /// Метод для показа результатов раунда квиза
-    private func show(quiz result: QuizResultsViewModel) {
+    func show(quiz result: QuizResultsViewModel) {
         //0 convert QuizResultsViewModel object to AlertModel object
         var alertModel = AlertViewModel(
             title: result.title,
@@ -135,8 +84,8 @@ final class MovieQuizViewController: UIViewController {
             guard let self = self else { print("Can't assign proper handler for alertModel"); return}
             // код, который сбрасывает игру и показывает первый вопрос
             self.presenter.resetQuestionIndex()
-            self.correctAnswers = 0
-            self.questionFactory?.requestNextQuestion()
+            self.presenter.correctAnswers = 0
+            self.presenter.questionFactory?.requestNextQuestion()
         }
         //1 init alert object
         alertView = AlertPresenter(
@@ -146,11 +95,11 @@ final class MovieQuizViewController: UIViewController {
         //2) Показть алерт при помощи паттерна "Делегат"
         alertView?.show()
     }
-    private func showLoadingIndicator() {
+    func showLoadingIndicator() {
         downloadMoviesIndicator.isHidden = false // говорим, что индикатор загрузки не скрыт
         downloadMoviesIndicator.startAnimating() // включаем анимацию
     }
-    private func showNetworkError(message: String) {
+    func showNetworkError(message: String) {
         downloadMoviesIndicator.isHidden = true
     
         var alertModel = AlertViewModel(
@@ -164,26 +113,6 @@ final class MovieQuizViewController: UIViewController {
                                   alertSome: alertModel
                                   )
         alertView?.show()
-    }
-}
-extension MovieQuizViewController: QuestionFactoryDelegate {
-    func didLoadDataFromServer() {
-        downloadMoviesIndicator.isHidden = true // скрываем индикатор загрузки
-        questionFactory?.requestNextQuestion()
-    }
-    
-    func didFailToLoadData(with error: Error) {
-        showNetworkError(message: error.localizedDescription)
-        //localizedDescription - Retrieve(Извлекать) the localized description for this error.
-    }
-    
-    func didReceiveNextQuestion(question: QuizQuestion?) {
-        guard let question = question else { return }
-            currentQuestion = question
-        let viewModel = presenter.convert(model: question)
-            DispatchQueue.main.async { [weak self] in
-                self?.show(quiz: viewModel)
-            }
     }
 }
 extension MovieQuizViewController: AlertPresenterDelegate {
