a2f35828964933ac945b27a55b7efdc30ce25792
Alexander <vinsanity305@mail.ru>
2023-05-24 13:10:39 +0300

    Done: started to refactor ViewController to MVP architecture.moved `function convert` For: to reduce amount of ViewController's responsibilities.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -8,12 +8,11 @@ final class MovieQuizViewController: UIViewController {
     // 3) Что значит "final class"?
     //4 Can I leave some commands, needed only for iOS-developer? (like `printSandBox()`)
     //! End of `Discuss`
+    private let presenter = MovieQuizPresenter()
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    private var currentQuestionIndex = 0 // переменная с индексом текущего вопроса
     private var correctAnswers = 0 // переменная с количеством правильных ответов
-    private let questionsAmount: Int = 10
     private var questionFactory: QuestionFactoryProtocol?
     private var currentQuestion: QuizQuestion?
     private var alertView: AlertPresenterProtocol?
@@ -26,13 +25,6 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var titleYesButton: UIButton!
     @IBOutlet private weak var downloadMoviesIndicator: UIActivityIndicatorView!
     
-    private func convert(model: QuizQuestion) -> QuizStepViewModel { // Конвертация вопроса в ViewModel
-        let questionStep = QuizStepViewModel(
-              image: UIImage(data: model.image) ?? UIImage(),
-              question: model.text,
-              questionNumber: "\(currentQuestionIndex+1)/\(questionsAmount)")
-        return questionStep
-    }
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -92,7 +84,7 @@ final class MovieQuizViewController: UIViewController {
     }
     private func showNextQuestionOrResults() {
         //переход в один из сценариев
-        if currentQuestionIndex == questionsAmount - 1 {
+        if presenter.isLastQuestion() {
 // состояние "Результат квиза"
 //1 Предварительная подготовка параметров модели "QuizResultsViewModel"
             //распаковка опционала, чтобы далее код был проще
@@ -100,7 +92,7 @@ final class MovieQuizViewController: UIViewController {
          // 1 рекорд
           // 1 сохранение лучшей игры
               // 1 запись текущей игры
-            let currentGame = GameRecord(correct: correctAnswers, total: questionsAmount, date: Date())
+            let currentGame = GameRecord(correct: correctAnswers, total: presenter.questionsAmount, date: Date())
               // 2 запись лучшей игры на устройство
             statistics.setBestGame(currentGame)
           // 2 сам рекорд - запись о лучшей игре
@@ -110,14 +102,14 @@ final class MovieQuizViewController: UIViewController {
                                  """
         //2 средняя точность
             //1 учесть в статистике по всем играм кол-во всех ответов, кол-во только правильных ответов, которые даны в последней игре
-            statistics.store(totalCurrent: questionsAmount, correctCurrent: correctAnswers)
+            statistics.store(totalCurrent: presenter.questionsAmount, correctCurrent: correctAnswers)
             //2 получить значение в виде, требуемом макетом
             let averageAccuracy = String(format: "%.2f", statistics.totalAccuracy)
 //2 Создание объекта "QuizResultsViewModel"
                 let result = QuizResultsViewModel(
                     title: "Этот раунд окончен!",
                     text: """
-                          Ваш результат: \(correctAnswers)/\(questionsAmount)
+                          Ваш результат: \(correctAnswers)/\(presenter.questionsAmount)
                           Количество сыгранных квизов: \(statistics.gamesCount)
                           Рекорд: \(record)
                           Средняя точность: \(averageAccuracy)%
@@ -128,7 +120,7 @@ final class MovieQuizViewController: UIViewController {
                 show(quiz: result)
         } else {
             //состояние "Следующий вопрос показан"
-            currentQuestionIndex += 1
+            presenter.switchToNextQuestion()
             questionFactory?.requestNextQuestion()
         }
     }
@@ -143,7 +135,7 @@ final class MovieQuizViewController: UIViewController {
         alertModel.handler = { [weak self] _ in
             guard let self = self else { print("Can't assign proper handler for alertModel"); return}
             // код, который сбрасывает игру и показывает первый вопрос
-            self.currentQuestionIndex = 0
+            self.presenter.resetQuestionIndex()
             self.correctAnswers = 0
             self.questionFactory?.requestNextQuestion()
         }
@@ -167,9 +159,7 @@ final class MovieQuizViewController: UIViewController {
             message: "Невозможно загрузить данные",
             buttonText: "Попробовать ещё раз"
         )
-        alertModel.handler = { [weak self] _ in
-            guard let self = self else { print("Can't assign proper handler for alertModel"); return}
-        }
+        alertModel.handler = { _ in }
         alertView = AlertPresenter(
                                   delegate: self,
                                   alertSome: alertModel
@@ -191,7 +181,7 @@ extension MovieQuizViewController: QuestionFactoryDelegate {
     func didReceiveNextQuestion(question: QuizQuestion?) {
         guard let question = question else { return }
             currentQuestion = question
-            let viewModel = convert(model: question)
+        let viewModel = presenter.convert(model: question)
             DispatchQueue.main.async { [weak self] in
                 self?.show(quiz: viewModel)
             }
